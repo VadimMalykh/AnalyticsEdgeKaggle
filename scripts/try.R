2169af7ae97a03d73536b10c3c4eea4d0991328e
Vadim Malykh <vadim.malykh@gmail.com>
2015-07-19 23:05:09 +1000

    Try script

---
+++ scripts/try.R
@@ -0,0 +1,73 @@
+# just try different models and features
+
+library(randomForest)
+library(caTools)
+library(ROCR)
+library(tm)
+library(SnowballC)
+library(cluster)
+
+train = read.csv("eBayiPadTrain.csv", stringsAsFactors = FALSE, fileEncoding="latin1")
+test = read.csv("eBayiPadTest.csv", stringsAsFactors = FALSE, fileEncoding="latin1")
+
+test$sold = NA
+train$is.train = 1 
+test$is.train = 0
+data = rbind(train, test)
+rm(train, test)
+
+corpus = Corpus(VectorSource(data$description))
+rm(for_corpus)
+corpus = tm_map(corpus, tolower)
+corpus = tm_map(corpus, PlainTextDocument)
+corpus = tm_map(corpus, removePunctuation)
+corpus = tm_map(corpus, removeWords, stopwords("english"))
+corpus = tm_map(corpus, removeWords, c("ipad"))
+corpus = tm_map(corpus, stemDocument)
+
+frequencies = DocumentTermMatrix(corpus)
+sparse = removeSparseTerms(frequencies, 0.99)
+rm(corpus, frequencies)
+
+words = as.data.frame(as.matrix(sparse))
+colnames(words) = make.names(colnames(words))
+data = cbind(data, words)
+rm(words, sparse)
+
+data$description = NULL
+data$condition = as.factor(data$condition)
+data$cellular = as.factor(data$cellular)
+data$carrier = as.factor(data$carrier)
+data$color = as.factor(data$color)
+data$storage = as.factor(data$storage)
+data$productline = as.factor(data$productline)
+
+train = subset(data, is.train == 1)
+test = subset(data, is.train == 0)
+
+train$UniqueID = NULL
+train$is.train = NULL
+
+set.seed(123)
+spl = sample.split(train$sold, SplitRatio = .8)
+val = subset(train, spl == FALSE)
+train = subset(train, spl == TRUE)
+
+# simple glm models
+lmodel1 = glm(sold~startprice, data=train, family=binomial)
+lpred1 = predict(lmodel1, newdata=val, type="response")
+ROCRpred = prediction(lpred1, val$sold)
+ROCRperf = performance(ROCRpred, "auc")
+ROCRperf # 0.7850872
+
+lmodel2 = glm(sold~startprice+biddable, data=train, family=binomial)
+lpred2 = predict(lmodel2, newdata=val, type="response")
+ROCRpred = prediction(lpred2, val$sold)
+ROCRperf = performance(ROCRpred, "auc")
+ROCRperf # 0.8333866
+
+lmodel3 = glm(sold~startprice+biddable+productline, data=train, family=binomial)
+lpred3 = predict(lmodel3, newdata=val, type="response")
+ROCRpred = prediction(lpred3, val$sold)
+ROCRperf = performance(ROCRpred, "auc")
+ROCRperf # 0.8516279
