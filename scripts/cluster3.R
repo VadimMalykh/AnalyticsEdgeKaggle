149749249311dd31bf700214d88d00ac9476fcce
Vadim Malykh <vadim.malykh@gmail.com>
2015-07-20 22:13:27 +1000

    new tries with clusters

---
+++ scripts/cluster3.R
@@ -0,0 +1,52 @@
+# Clustering 
+# test - ?
+# train - ?
+
+library(tm)
+library(SnowballC)
+library(caTools)
+library(ROCR)
+library(randomForest)
+
+train = read.csv("eBayiPadTrain.csv", stringsAsFactors = FALSE)
+test = read.csv("eBayiPadTest.csv", stringsAsFactors = FALSE)
+
+test$sold = NA
+train$is.train = 1 
+test$is.train = 0
+data = rbind(train, test)
+rm(train, test)
+
+corpus = Corpus(VectorSource(data$description))
+corpus = tm_map(corpus, tolower)
+corpus = tm_map(corpus, PlainTextDocument)
+corpus = tm_map(corpus, removePunctuation)
+corpus = tm_map(corpus, removeWords, stopwords("english"))
+corpus = tm_map(corpus, removeWords, c("ipad"))
+corpus = tm_map(corpus, stemDocument)
+
+frequencies = DocumentTermMatrix(corpus)
+sparse = removeSparseTerms(frequencies, 0.985)
+rm(corpus, frequencies)
+
+words = as.data.frame(as.matrix(sparse))
+colnames(words) = make.names(colnames(words))
+data = cbind(data, words)
+rm(words, sparse)
+
+data$description = NULL
+data$condition = as.factor(data$condition)
+data$cellular = as.factor(data$cellular)
+data$carrier = as.factor(data$carrier)
+data$color = as.factor(data$color)
+data$storage = as.factor(data$storage)
+data$productline = as.factor(data$productline)
+
+# k-means clustering by words
+for_clusters = data[,12:ncol(data)]
+km = kmeans(for_clusters, centers = 4)
+group1 = subset(data, km$cluster >= 1 & km$cluster <= 3)
+group2 = subset(data, km$cluster == 4)
+
+wordcloud(colnames(group1[,12:55]), colSums(group1[,12:55]), scale=c(2,.25))
+wordcloud(colnames(group2[,12:55]), colSums(group2[,12:55]), scale=c(2,.25))
