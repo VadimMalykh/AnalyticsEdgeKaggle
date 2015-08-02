71d4ec0d40035c44f96774478e9fb911e4391a07
Vadim Malykh <vadim.malykh@gmail.com>
2015-08-02 21:17:48 +1000

    try 5

---
+++ scripts/try5.R
@@ -0,0 +1,57 @@
+# Try PCA + cv on randomFores and glm
+# test - ?
+# train - ?
+
+Sys.setlocale("LC_ALL", "C")
+
+library(tm)
+library(SnowballC)
+library(caTools)
+library(ROCR)
+library(randomForest)
+
+train = read.csv("eBayiPadTrain.csv", stringsAsFactors = FALSE)
+test = read.csv("eBayiPadTest.csv", stringsAsFactors = FALSE)
+
+test$sold = NA
+train$is.train = 1 
+test$is.train = 0
+data = rbind(train, test)
+rm(train, test)
+
+corpus = Corpus(VectorSource(data$description))
+corpus = tm_map(corpus, tolower)
+corpus = tm_map(corpus, PlainTextDocument)
+corpus = tm_map(corpus, removePunctuation)
+corpus = tm_map(corpus, removeWords, stopwords("english"))
+corpus = tm_map(corpus, removeWords, c("ipad"))
+corpus = tm_map(corpus, stemDocument)
+
+frequencies = DocumentTermMatrix(corpus)
+
+words = as.data.frame(as.matrix(frequencies))
+colnames(words) = make.names(colnames(words))
+data = cbind(data, words)
+
+data$description = NULL
+data$condition = as.factor(data$condition)
+data$cellular = as.factor(data$cellular)
+data$carrier = as.factor(data$carrier)
+data$color = as.factor(data$color)
+data$storage = as.factor(data$storage)
+data$productline = as.factor(data$productline)
+
+Train = subset(data, is.train == 1)
+Test = subset(data, is.train == 0)
+
+Train$is.train = NULL
+Train$UniqueID = NULL
+
+#PCA
+
+#CV on RF
+
+#CV on glm
+
+submission = data.frame(UniqueID = Test$UniqueID, Probability1 = pred)
+write.csv(submission, "submission.csv", row.names = FALSE)
